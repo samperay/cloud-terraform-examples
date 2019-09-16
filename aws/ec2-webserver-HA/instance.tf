4d903355b2542d25255c4d47bc613fc174806c6a
samperay <sunlnx@gmail.com>
2019-09-16 14:50:25 +0530

    modifed count parameter and user data to reflect ip changes of the instance

--- aws/ec2-webserver-HA/instance.tf
+++ aws/ec2-webserver-HA/instance.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "web" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
   key_name      = "${aws_key_pair.mykey.key_name}"
+  count         = 2
+
+  user_data = "#!/bin/bash\nsudo apt-get install apache2 -y\nsudo systemctl start apache2\nsudo curl http://169.254.169.254/latest/meta-data/public-ipv4 >index.html\nsudo cp ./index.html /var/www/html/index.html"
 
   tags = {
     Name = "web"
