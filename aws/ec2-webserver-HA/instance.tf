6cdcb0e77ae05512a52c1c6d971a4cd7b91df44d
samperay <sunlnx@gmail.com>
2019-09-16 15:14:39 +0530

    userdata removed and added in script folder

--- aws/ec2-webserver-HA/instance.tf
+++ aws/ec2-webserver-HA/instance.tf
@@ -4,7 +4,23 @@ resource "aws_instance" "web" {
   key_name      = "${aws_key_pair.mykey.key_name}"
   count         = 2
 
-  user_data = "#!/bin/bash\nsudo apt-get install apache2 -y\nsudo systemctl start apache2\nsudo curl http://169.254.169.254/latest/meta-data/public-ipv4 >index.html\nsudo cp ./index.html /var/www/html/index.html"
+  provisioner "file" {
+      source = "scripts/script.sh"
+      destination = "/tmp/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo /tmp/script.sh | tee script-output.log"
+    ]
+  }
+
+  connection {
+    host = "${self.public_ip}"
+    user = "ubuntu"
+    private_key = "${file("${var.PRIVATE_KEY}")}"
+  }
 
   tags = {
     Name = "web"
