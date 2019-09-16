8e6594c104dd0506d68f2b924d0198d694525bf1
samperay <sunlnx@gmail.com>
2019-09-16 15:14:10 +0530

    added inside scripts folder

--- aws/ec2-webserver-HA/scripts/script.sh
+++ aws/ec2-webserver-HA/scripts/script.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get install apache2 -y
 sudo systemctl start apache2 
 curl http://169.254.169.254/latest/meta-data/public-ipv4 > index.html 
-sudo mv index.html /var/www/html/index.html
+sudo mv index.html /var/www/html/index.html 
