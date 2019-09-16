2be4dab48cc12bdf325ba0c092ac5706447e57a1
samperay <sunlnx@gmail.com>
2019-09-16 18:04:17 +0530

    added image folder for arch diagram

--- aws/ec2-webserver-HA/scripts/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install apache2 -y
-sudo systemctl start apache2 
-curl http://169.254.169.254/latest/meta-data/public-ipv4 > index.html 
-sudo mv index.html /var/www/html/index.html 
