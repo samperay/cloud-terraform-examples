7fc22c76f8299e4e13e6e4317a6e449610ee34f8
samperay <sunlnx@gmail.com>
2019-09-16 17:15:27 +0530

    added user_data to group

--- aws/ec2-webserver-HA/autoscaling-launch-config.tf
+++ aws/ec2-webserver-HA/autoscaling-launch-config.tf
@@ -4,10 +4,12 @@ resource "aws_launch_configuration" "web-launch" {
   instance_type   = "t2.micro"
   key_name        = "${aws_key_pair.mykey.key_name}"
   security_groups = ["${aws_security_group.web.id}"]
+  user_data       = "#!/bin/bash\nsudo apt-get update\nsudo apt-get install nginx -y\nIP=`sudo ifconfig | grep 'addr:10' | awk '{ print $2 }' | cut -d ':' -f2`\n echo 'this is instance: ' $IP > /var/www/html/index.html"
+  
 }
 
 resource "aws_autoscaling_group" "web-autoscaling" {
-  name                      = "example-autoscaling"
+  name                      = "web-autoscaling"
   vpc_zone_identifier       = ["${aws_subnet.main-subnet-public-1.id}", "${aws_subnet.main-subnet-public-2.id}"]
   launch_configuration      = "${aws_launch_configuration.web-launch.name}"
   min_size                  = 2
@@ -23,7 +25,7 @@ resource "aws_autoscaling_group" "web-autoscaling" {
 
   tag {
     key                 = "Name"
-    value               = "ec2-instance"
+    value               = "web-instance"
     propagate_at_launch = true
   }
 }
