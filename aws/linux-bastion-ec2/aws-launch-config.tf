077e1c803cc7fbf000eab35941d6e71cc542136a
samperay <sunlnx@gmail.com>
2019-09-17 18:20:42 +0530

    modied sg group

--- aws/linux-bastion-ec2/aws-launch-config.tf
+++ aws/linux-bastion-ec2/aws-launch-config.tf
@@ -5,5 +5,5 @@ resource "aws_launch_configuration" "bastion-lc" {
   image_id        = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type   = "t2.micro"
   key_name        = "${aws_key_pair.mykey.key_name}"
-  security_groups = ["${aws_security_group.bastion-allow-ssh.id}"]
+  security_groups = ["${aws_security_group.bastion-allow-public.id}"]
 }
