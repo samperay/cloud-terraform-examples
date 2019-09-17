38372ccaa9b2345ee0474eb03d30b0d037ccd335
samperay <sunlnx@gmail.com>
2019-09-17 12:07:43 +0530

    name confict with name_prefix rectified

--- aws/linux-bastion-ec2/autoscalinggroup.tf
+++ aws/linux-bastion-ec2/autoscalinggroup.tf
@@ -1,7 +1,6 @@
 // Creates autoscaling group for bastion hosts
 resource "aws_autoscaling_group" "bastion-asg" {
   name_prefix          = "linux-bastion"
-  name                 = "linux-bastion"
   max_size             = 3
   min_size             = 2
   desired_capacity     = 2
@@ -9,10 +8,12 @@ resource "aws_autoscaling_group" "bastion-asg" {
   health_check_type    = "EC2"
   vpc_zone_identifier  = ["${aws_subnet.bastion-public-1.id}", "${aws_subnet.bastion-public-2.id}"]
 
-  tags = {
-    key                 = "Name"
-    value               = "linux-bastion"
-    propagate_at_launch = true
-  }
+  tags = [ 
+    {
+      key                 = "Name"
+      value               = "linux-bastion"
+      propagate_at_launch = true
+    } 
+  ]
 
 }
