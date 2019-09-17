8b899c577bffafdd4bafa2437a77c7265d483399
samperay <sunlnx@gmail.com>
2019-09-17 10:31:08 +0530

    networking files for project added

---
+++ aws/Linux Bastion EC2/nat.tf
@@ -0,0 +1,55 @@
+// Create an elastic IP address for NAT Gateway
+resource "aws_eip" "eip-nat" {
+    vpc = true
+}
+
+// Create a NAT gateway 
+
+resource "aws_nat_gateway" "natgw" {
+    allocation_id = "${aws_eip.eip-nat.id}"
+    subnet_id = "${aws_subnet.bastion-public-1.id}"
+
+    tags = {
+        Name = "natgw"
+    }
+}
+
+// Create private route
+
+resource "aws_route_table" "bastion-route-private-gw-1" {
+  vpc_id = "${aws_vpc.bastion.id}"
+
+  route {
+      cidr_block = "10.0.0.0/19"
+      gateway_id = "${aws_nat_gateway.natgw.id}"
+  }
+
+    tags = {
+        Name = "bastion-route-private-gw-1"
+    }
+}
+
+resource "aws_route_table" "bastion-route-private-gw-2" {
+  vpc_id = "${aws_vpc.bastion.id}"
+
+  route {
+      cidr_block = "10.0.32.0/19"
+      gateway_id = "${aws_nat_gateway.natgw.id}"
+  }
+
+    tags = {
+        Name = "bastion-route-private-gw-2"
+    }
+}
+
+// associate private route to subnet
+
+resource "aws_route_table_association" "bastion-private-route assoc-1" {
+    subnet_id = "${aws_subnet.bastion-private-1.id}"
+    route_table_id = "${aws_route_table.bastion-route-private-gw-1.id}"
+}
+
+resource "aws_route_table_association" "bastion-private-route assoc-2" {
+    subnet_id = "${aws_subnet.bastion-private-2.id}"
+    route_table_id = "${aws_route_table.bastion-route-private-gw-2.id}"
+}
---
+++ aws/Linux Bastion EC2/vpc.tf
@@ -0,0 +1,95 @@
+resource "aws_vpc" "bastion" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+
+    tags =  {
+        Name = "bastion"
+    }
+}
+
+// create public subnet
+
+resource "aws_subnet" "bastion-public-1" {
+    vpc_id = "${aws_vpc.bastion.id}"
+    availability_zone = "ap-south-1a"
+    cidr_block = "10.0.128.0/20"
+    map_public_ip_on_launch = "false"
+
+    tags = {
+        Name = "bastion-public-1"
+    }
+}
+
+resource "aws_subnet" "bastion-public-2" {
+    vpc_id = "${aws_vpc.bastion.id}"
+    availability_zone = "ap-south-1b"
+    cidr_block = "10.0.144.0/20"
+    map_public_ip_on_launch = "false"
+
+    tags = {
+        Name = "bastion-public-b"
+    }
+}
+
+// create private subnet
+
+resource "aws_subnet" "bastion-private-1" {
+  vpc_id = "${aws_vpc.bastion.id}"
+  cidr_block = "10.0.0.0/19"
+  map_public_ip_on_launch = "false"
+  availability_zone = "ap-south-1a"
+
+    tags = {
+        Name = "bastion-private-1"
+    }
+}
+
+resource "aws_subnet" "bastion-private-2" {
+  vpc_id = "${aws_vpc.bastion.id}"
+  cidr_block = "10.0.32.0/19"
+  map_public_ip_on_launch = "false"
+  availability_zone = "ap-south-1b"
+
+    tags = {
+        Name = "bastion-private-2"
+    }
+}
+
+// crate Internet Gateway
+resource "aws_internet_gateway" "bastion-public-igw" {
+    vpc_id = "${aws_vpc.bastion.id}"
+
+    tags = {
+        Name = "bastion-public-igw"
+    }
+}
+
+// Create public route 
+resource "aws_route_table" "bastion-route-public-1" {
+    vpc_id = "${aws_vpc.bastion.id}"
+
+    route {
+        cidr_block = "10.0.128.0/20"
+        gateway_id = "${aws_internet_gateway.bastion-public-igw}"
+    }    
+}
+
+resource "aws_route_table" "bastion-route-public-1" {
+    vpc_id = "${aws_vpc.bastion.id}"
+
+    route {
+        cidr_block = "10.0.144.0/20"
+        gateway_id = "${aws_internet_gateway.bastion-public-igw}"
+    }  
+}
+
+// associate subnet to public route table
+resource "aws_route_table_association" "bastion-public-route assoc-1" {
+    subnet_id = "${aws_subnet.bastion-public-1.id}"
+    route_table_id = "${aws_route_table.bastion-route-public-1.id}"
+}
+
+resource "aws_route_table_association" "bastion-public-route assoc-2" {
+    subnet_id = "${aws_subnet.bastion-public-2.id}"
+    route_table_id = "${aws_route_table.bastion-route-public-2.id}"
+}
