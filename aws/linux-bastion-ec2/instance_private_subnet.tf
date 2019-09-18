14f426488541eccba657de6c5725ecc5bd4939af
samperay <sunlnx@gmail.com>
2019-09-18 09:39:44 +0530

    syntax errors fixed

--- aws/linux-bastion-ec2/instance_private_subnet.tf
+++ aws/linux-bastion-ec2/instance_private_subnet.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "private-instance" {
    ami =  "${lookup(var.AMIS, var.AWS_REGION)}"
    instance_type = "t2.micro"
    key_name = "${aws_key_pair.mykey.key_name}"
-   security_groups = "${aws_security_group.bastion-allow-private.id}"
-   subnet_id = ["${aws_subnet.bastion-private-1.cidr_block}"]
+   security_groups = ["${aws_security_group.bastion-allow-private.id}"]
+   subnet_id = "${aws_subnet.bastion-private-1.cidr_block}"
 
    tags = {
        Name = "Private-instance"
