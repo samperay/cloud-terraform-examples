8dc83e1f8b59b124ac26fd555427a70d21fd03b3
samperay <sunlnx@gmail.com>
2019-09-18 09:52:06 +0530

    fixed subnet group id correctly

--- aws/linux-bastion-ec2/instance_private_subnet.tf
+++ aws/linux-bastion-ec2/instance_private_subnet.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "private-instance" {
    instance_type = "t2.micro"
    key_name = "${aws_key_pair.mykey.key_name}"
    security_groups = ["${aws_security_group.bastion-allow-private.id}"]
-   subnet_id = "${aws_subnet.bastion-private-1.cidr_block}"
+   subnet_id = "${aws_subnet.bastion-private-1.id}"
 
    tags = {
        Name = "Private-instance"
