a3eeed805b4b82931a30fae4ce3501ce59fddb26
samperay <sunlnx@gmail.com>
2019-09-20 20:09:04 +0530

    modified userdata in the instance while it boots

--- aws/EC2-Simple-WebServer-Example/Instance.tf
+++ aws/EC2-Simple-WebServer-Example/Instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "example" {
     ami = "${lookup(var.AMIS, var.AWS_REGION)}"
     instance_type = "t2.micro"
     key_name = "${aws_key_pair.mykey.key_name}"
-    user_data = "#!/bin/bash\nsudo yum install -y httpd\nsudo systemctl start httpd\ncd /var/www/html\nsudo echo '<html><h1>Hello, Welcome to AWS test Instance</h1></html>'>index.html`"
+    user_data = "#!/bin/bash\nsudo yum install -y httpd\nsudo service httpd start\ncd /var/www/html\nsudo chmod 777 /var/www/html\nsudo echo '<html><h1>Hello, Welcome to AWS test Instance</h1></html>'>index.html`"
 
     tags = {
         Name = "test-example"
