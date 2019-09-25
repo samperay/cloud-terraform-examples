7268412384f958efdb73304354b9f550baaf7230
samperay <sunlnx@gmail.com>
2019-09-25 20:34:39 +0530

    default values for aws to work

---
+++ aws/EC2-WebServer-Custom-CloudWatch/variable.tf
@@ -0,0 +1,24 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {  
+}
+
+variable "AWS_REGION" {
+    default = "ap-south-1"
+}
+
+variable "AMIS" {
+  type = "map"
+  default ={
+      ap-south-1 = "ami-0912f71e06545ad88"
+  }
+}
+
+variable "PUBLIC_KEY" {
+  default ="mykey.pub"
+}
+
+variable "PRIVATE_KEY" {
+  default = "mykey"
+}
