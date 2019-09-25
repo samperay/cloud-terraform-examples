c540ab4352b7bf00738eaeacfc77a5131633b689
samperay <sunlnx@gmail.com>
2019-09-25 20:35:03 +0530

    keypair and security groups created

---
+++ aws/EC2-WebServer-Custom-CloudWatch/securitygroup.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "example-allow-http" {
+    name = "example-allow-http"
+    description = "allow http port"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "example-allow-http"
+    }
+}
