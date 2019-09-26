80a0ed817353481c50899e69224463a06f84a741
samperay <sunlnx@gmail.com>
2019-09-26 19:12:33 +0530

    removed 'roles' to 'role' as its being depreciated

--- aws/EC2-WebServer-Custom-CloudWatch/create-iam-instance-profile-ec2.tf
+++ aws/EC2-WebServer-Custom-CloudWatch/create-iam-instance-profile-ec2.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_instance_profile" "cw-instance-profile" {
   name  = "cw-instance-profile"
-  roles = ["${aws_iam_role.ec2_cloudwatch_access_role.name}"]
+  role  = "${aws_iam_role.ec2_cloudwatch_access_role.name}"
 }
