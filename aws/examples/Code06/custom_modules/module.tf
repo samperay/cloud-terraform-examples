d2f53e94822d68e021e174a8ee27d38edce3b5be
samperay <sunlnx@gmail.com>
2020-05-23 18:36:52 +0530

    removed aws creds and using profile

--- aws/examples/Code06/custom_modules/module.tf
+++ aws/examples/Code06/custom_modules/module.tf
@@ -1,12 +1,7 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  profile = "default"
   region = "${var.aws_region}"
 }
-
-variable "aws_access_key" {}
-
-variable "aws_secret_key" {}
 
 variable "aws_region" {
   default="ap-south-1"
