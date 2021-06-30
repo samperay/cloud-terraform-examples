3688969ebb6ea204fcc3e5cb9463966fa870b9d5
samperay <sunlnx@gmail.com>
2021-06-30 18:14:29 +0530

    counting list of instances in the AZ

--- aws/tf-courses/tf_up_running/chap02/one-webserver/main.tf
+++ aws/tf-courses/tf_up_running/chap02/one-webserver/main.tf
@@ -2,7 +2,15 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+data "aws_availability_zones" "all" {}
+
 resource "aws_instance" "example" {
+  # get list of AZ 
+
+  # count = length(data.aws_availability_zones.all.names)
+  # availability_zone = data.aws_availability_zones.all.names[count.index]
+
+  # make sure the AMI is available in all the 3 AZ's and then you can un-comment
   ami = "ami-0c1a7f89451184c8b"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance-sg.id]
@@ -25,6 +33,4 @@ resource "aws_security_group" "instance-sg" {
     protocol = "tcp"
     cidr_blocks = [ "0.0.0.0/0" ]
   }
-
-  
 }
