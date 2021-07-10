d3dacbe1d5e8882e645e5be79fc39293af72793f
samperay <sunlnx@gmail.com>
2021-07-10 18:14:38 +0530

    ibm first terraform files

---
+++ ibm/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.21.2"
+    }
+  }
+}
+
+# Configure the IBM Provider
+provider "ibm" {
+  region = "us-south"
+}
---
+++ ibm/vpc.tf
@@ -0,0 +1,4 @@
+# Create a VPC
+resource "ibm_is_vpc" "testacc_vpc" {
+  name = "test-vpc"
+}
