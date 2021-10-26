fed04246485f63d608ae70317a7e1fcda18e0536
samperay <sunlnx@gmail.com>
2021-10-26 12:13:21 +0530

    create module of resource groups

---
+++ ibm/modules/resource_groups/main.tf
@@ -0,0 +1,3 @@
+resource "ibm_resource_group" "example_rg" {
+  name = "tf_testing_rg"
+}
---
+++ ibm/modules/resource_groups/output.tf
@@ -0,0 +1,4 @@
+output "rg_id" {
+  value       = ibm_resource_group.example_rg.id
+  description = "rg id: "
+}
